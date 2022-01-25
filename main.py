1aa1d36e0396a87e8c38a8d1f44e07a6ba5567c0
Meeper <meeper@protonmail.com>
2022-01-24 22:56:12 -0600

    Use temp db, added second file read, print db

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect("test.db")
+connection = sqlite3.connect("")
 cursor = connection.cursor()
 cursor.execute("CREATE TABLE messages (timestamp TEXT, message TEXT)")
 file1 = open('a.txt', 'r')
@@ -14,10 +14,22 @@ while True:
         break
     cursor.execute("INSERT INTO messages VALUES (?, ?)", (line[0:line.find(']  ')+1], line[line.find(']  ')+3:-1]))
 
-connection.commit()
-
 file1.close()
 file1 = open('b.txt', 'r')
 
-rows = cursor.execute('SELECT COUNT(*) FROM messages')
-print(rows.rowcount)
+while True: 
+    # Get next line from file
+    line = file1.readline()
+    # if line is empty
+    # end of file is reached
+    if not line:
+        break
+    cursor.execute("INSERT INTO messages VALUES (?, ?)", (line[0:line.find(']  ')+1], line[line.find(']  ')+3:-1]))
+
+file1.close()
+connection.commit()
+
+rows = cursor.execute('SELECT timestamp, message FROM messages').fetchall()
+cursor.close()
+for a in rows:
+    print(a[0] + " " + a[1])
