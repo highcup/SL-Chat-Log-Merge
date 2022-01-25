cfe22898b078d27b8a0197797bfb1df5c036ae41
Meeper <meeper@protonmail.com>
2022-01-24 23:13:56 -0600

    seperate into functions

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
 import sqlite3
 
+def createDB (cursor) :
+    cursor.execute("CREATE TABLE messages (timestamp TEXT, message TEXT)")
+    connection.commit()
+
+def loadMessages (cursor, file) :
+    for line in file :
+        cursor.execute("INSERT INTO messages VALUES (?, ?)", (line[0:line.find(']  ')+1], line[line.find(']  ')+3:-1]))
+    connection.commit()
+
+def printMessages (cursor) :
+    data = cursor.execute("SELECT timestamp, message FROM messages").fetchall()
+    for row in data :
+        print(row[0] + " " + row[1])
+
 connection = sqlite3.connect("")
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE messages (timestamp TEXT, message TEXT)")
-file1 = open('a.txt', 'r')
-
-while True: 
-    # Get next line from file
-    line = file1.readline()
-    # if line is empty
-    # end of file is reached
-    if not line:
-        break
-    cursor.execute("INSERT INTO messages VALUES (?, ?)", (line[0:line.find(']  ')+1], line[line.find(']  ')+3:-1]))
-
-file1.close()
-file1 = open('b.txt', 'r')
-
-while True: 
-    # Get next line from file
-    line = file1.readline()
-    # if line is empty
-    # end of file is reached
-    if not line:
-        break
-    cursor.execute("INSERT INTO messages VALUES (?, ?)", (line[0:line.find(']  ')+1], line[line.find(']  ')+3:-1]))
-
-file1.close()
-connection.commit()
-
-rows = cursor.execute('SELECT timestamp, message FROM messages').fetchall()
-cursor.close()
-for a in rows:
-    print(a[0] + " " + a[1])
+createDB(cursor)
+file = open('a.txt', 'r')
+loadMessages(cursor, file)
+file.close()
+file = open('b.txt', 'r')
+loadMessages(cursor, file)
+file.close()
+printMessages(cursor)
+connection.close()
